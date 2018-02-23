4d46713cfd33a910187374f05d6249dd5a8236bf
padmacorps <padmacorps@gmail.com>
2018-02-23 15:28:12 +0700

    Create googleads.js

---
+++ googleads.js
@@ -0,0 +1,12 @@
+// JavaScript Document //<br />
+ads = new Array(4);<br />
+ads [0] = ‘</p>
+<div class="ads-box" "item"><a href="https://goo.gl/5A9Du3" target="blank"><img src="https://3.bp.blogspot.com/-26lTKaLXDtk/WjsSmMBLygI/AAAAAAAABxk/C37KkL_nXwEW0iDpZSARbEaBS3lTUHlSQCLcBGAs/s1600/wow.gif" data-recalc-dims="1"></a></p>
+<p class="ad"><a href="https://goo.gl/5A9Du3" target="blank"><span><font color="#000000" size="1" face="Arial, Helvetica, sans-serif">AdChoices</font></span></a></p>
+</div>
+<p>‘;<br />
+view = Math.floor(Math.random() * 1); document.write(ads[view]);
+
+
+
+ 
